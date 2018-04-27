eca19fbe7c477d85ed6f05be0f8d816b4c712064
Nick Miyake <nmiyake@palantir.com>
2018-04-27 13:54:19 -0700

    Initial commit

---
+++ cmd/plugininfo.go
@@ -0,0 +1,43 @@
+// Copyright (c) 2018 Palantir Technologies Inc. All rights reserved.
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+package cmd
+
+import (
+	"github.com/palantir/godel/framework/pluginapi/v2/pluginapi"
+	"github.com/palantir/godel/framework/verifyorder"
+)
+
+var (
+	Version    = "unspecified"
+	PluginInfo = pluginapi.MustNewPluginInfo(
+		"com.palantir.amalgomate",
+		"amalgomate-plugin",
+		Version,
+		pluginapi.PluginInfoUsesConfigFile(),
+		pluginapi.PluginInfoGlobalFlagOptions(
+			pluginapi.GlobalFlagOptionsParamDebugFlag("--"+pluginapi.DebugFlagName),
+			pluginapi.GlobalFlagOptionsParamProjectDirFlag("--"+pluginapi.ProjectDirFlagName),
+			pluginapi.GlobalFlagOptionsParamConfigFlag("--"+pluginapi.ConfigFlagName),
+		),
+		pluginapi.PluginInfoTaskInfo(
+			"amalgomate",
+			"Amalgomate programs",
+			pluginapi.TaskInfoCommand("run"),
+			pluginapi.TaskInfoVerifyOptions(
+				// by default, run after "generate" but before next built-in task
+				pluginapi.VerifyOptionsOrdering(intVar(verifyorder.Generate+50)),
+				pluginapi.VerifyOptionsApplyFalseArgs("--"+VerifyFlagName),
+			),
+		),
+		pluginapi.PluginInfoUpgradeConfigTaskInfo(
+			pluginapi.UpgradeConfigTaskInfoCommand("upgrade-config"),
+			pluginapi.LegacyConfigFile("amalgomate.yml"),
+		),
+	)
+)
+
+func intVar(val int) *int {
+	return &val
+}
---
+++ main.go
@@ -0,0 +1,20 @@
+// Copyright (c) 2018 Palantir Technologies Inc. All rights reserved.
+// Use of this source code is governed by the Apache License, Version 2.0
+// that can be found in the LICENSE file.
+
+package main
+
+import (
+	"os"
+
+	"github.com/palantir/godel/framework/pluginapi/v2/pluginapi"
+
+	"github.com/palantir/godel-amalgomate-plugin/cmd"
+)
+
+func main() {
+	if ok := pluginapi.InfoCmd(os.Args, os.Stdout, cmd.PluginInfo); ok {
+		return
+	}
+	os.Exit(cmd.Execute())
+}
